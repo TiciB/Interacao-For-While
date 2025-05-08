1cf34f4034908e34b3e5cffca40e8cd092ebd3bc
Jhamyllie <milli.santana2017@gmail.com>
2025-05-08 15:20:57 -0300

    Adicona Exrecício números entre M e N

--- src/implementacao/3-numeros-entre-m-e-n.ts
+++ src/implementacao/3-numeros-entre-m-e-n.ts
@@ -1,3 +1,15 @@
-function mostrarNumerosEntreMeN() {
-    return null;
+function mostrarNumerosEntreMeN(primeiroNumero: number, segundoNumero: number): number[] {
+    let numerosNoIntervalo: number [] = [];
+
+    for (let index = primeiroNumero; index < segundoNumero; index++) {
+        if(index % 2 === 1){
+            numerosNoIntervalo.push(index)
+        }
+    }
+    return numerosNoIntervalo
 }
+
+// console.log(mostrarNumerosEntreMeN(2, 10));
+// console.log(mostrarNumerosEntreMeN(3, 10));
+// console.log(mostrarNumerosEntreMeN(2, 5));
+
