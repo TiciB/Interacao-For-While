93edf368aedd831736999c601f32fb8495369983
TiciB <tebezerra@gmail.com>
2025-05-08 14:06:11 -0300

    Exercício numeros de 0 a N

--- src/implementacao/2-numeros-0-a-n.ts
+++ src/implementacao/2-numeros-0-a-n.ts
@@ -1,3 +1,13 @@
-function mostrarNumerosAteN() {
-    return null;
+function mostrarNumerosAteN(numero:number):number {
+    let index: number = 0;
+
+    for (index = 0; index <= numero; index++){
+        console.log(index);
+    }
+       
+    return numero;
 }
+
+//console.log(mostrarNumerosAteN(5));
+//console.log(mostrarNumerosAteN(10));
+//console.log(mostrarNumerosAteN(3));
