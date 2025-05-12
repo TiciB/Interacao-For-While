360e56a6a01836ffb6d937ec5cc8a2a6c67161c6
TiciB <tebezerra@gmail.com>
2025-05-12 10:43:57 -0300

    Exercício de  multiplos

--- src/implementacao/8-multiplos-de-2-e-5.ts
+++ src/implementacao/8-multiplos-de-2-e-5.ts
@@ -1,3 +1,28 @@
-function analiseMultiplos() {
-    return null;
+function analiseMultiplos(numeros:number[]):void {
+    let i: number = 0
+    let contador1: number = 0
+    let contador2: number = 0
+    for (i = 0; i < numeros.length; i++){
+         const numero = numeros[i];
+
+        if(numeros[i] === -1){
+            break;
+        }
+        if(numeros[i] % 2 == 0 && numeros[i] % 3 == 0){
+            contador1++
+        
+        }
+        if(numeros[i] % 2 == 0 && numeros[i] % 5 == 0){
+            contador2++
+       
+        }
+
+    }
+    console.log(`${contador1} numero(s) múltiplo(s) de 2 e 3` );
+    console.log(`${contador2} numero(s) múltiplo(s) de 2 e 5` );   
 }
+
+//let lista:number[] = [10, 15, 30, -1];
+//let lista:number[] = [6, 12, 20, 25, -1];
+//let lista:number[] = [10, 15, 30, -1];
+console.log(analiseMultiplos(lista));
