123f3d8d120fff27598bba13f12c63e5b826ce75
Jhamyllie <milli.santana2017@gmail.com>
2025-05-12 15:22:27 -0300

    Adiciona exercício Tabela polegadas  para centímetro.

--- src/implementacao/10-tabela-polegadas-para-cm.ts
+++ src/implementacao/10-tabela-polegadas-para-cm.ts
@@ -1,3 +1,29 @@
-function tabelaConversao() {
-    return null;
+function tabelaConversao(arrayNumber: number[]): {}[] {
+    // arrayNumber = [1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let tabela: {polegada: number, centímetro: number}[] = []
+
+    for(let i = 0; i < arrayNumber.length; i++){
+        let numeroEmPolegadas = arrayNumber[i];
+        let polegadasEmCentímetros = numeroEmPolegadas * 2.54;
+        tabela.push({polegada: numeroEmPolegadas, centímetro: polegadasEmCentímetros})
+    }
+
+    return tabela;
 }
+
+console.log(tabelaConversao([1, 2 ,3]));
+console.log(tabelaConversao([1, 2 ,3 ,4 ,5]));
+console.log(tabelaConversao([1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]));
+
+/*
+A tabela gerada para polegadas de 1 a 3 seria:
+1 pol = 2.54 cm
+2 pol = 5.08 cm
+3 pol = 7.62 cm
+A tabela gerada para polegadas de 1 a 5 seria:
+1 pol = 2.54 cm
+2 pol = 5.08 cm
+3 pol = 7.62 cm
+4 pol = 10.16 cm
+5 pol = 12.7 cm
+*/
